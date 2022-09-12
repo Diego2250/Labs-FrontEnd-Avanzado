feac156cfb4d833da4ddce3879883082584185ab
Diego Morales <mor21146@uvg.edu.gt>
2022-09-11 21:04:00 -0600

    Todos los visuales creados. falta backend

---
+++ Lab8/app/src/main/java/com/example/lab8/CharacterDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.example.lab8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CharacterDetailFragment : Fragment(R.layout.fragment_character_detail) {
+
+
+}
